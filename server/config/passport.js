a740b9af6ade5fd56a6c03092b487320282a03fd
James-Lee-code <acc22331@gmail.com>
2025-06-28 19:30:45 +0800

    前後端整合完成，準備部署

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,23 +1,24 @@
-let JwtStrategy = require("passport-jwt").Strategy;
-let ExtractJwt = require("passport-jwt").ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models").user;
 
 module.exports = (passport) => {
-  let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+  const opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
   opts.secretOrKey = process.env.PASSPORT_SECRET;
 
   passport.use(
-    new JwtStrategy(opts, async function (jwt_payload, done) {
+    new JwtStrategy(opts, async (jwt_payload, done) => {
       try {
-        let foundUser = await User.findOne({ _id: jwt_payload._id }).exec();
-        if (foundUser) {
-          return done(null, foundUser);
+        console.log("JWT Payload:", jwt_payload); // 確認內容
+        const user = await User.findById(jwt_payload.id).exec();
+        if (user) {
+          return done(null, user);
         } else {
           return done(null, false);
         }
-      } catch (e) {
-        return done(e, false);
+      } catch (error) {
+        return done(error, false);
       }
     })
   );
--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const app = express();
+const path = require("path");          
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const authRoute = require("./routes").auth;
-const courseRoute = require("./routes").course;
+
 const passport = require("passport");
 require("./config/passport")(passport);
+
 const cors = require("cors");
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("連結到mongodb..."))
-  .catch((e) => console.log(e));
+const authRoute = require("./routes").auth;
+const courseRoute = require("./routes").course;
 
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(passport.initialize());
+
 
 app.use("/api/user", authRoute);
 app.use(
@@ -26,6 +28,25 @@ app.use(
   courseRoute
 );
 
-app.listen(8080, () => {
-  console.log("後端伺服器聆聽在port 8080...");
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
+
+const PORT = process.env.PORT || 5000;
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("連結到 MongoDB...");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB 連線錯誤：", error);
+  });
